9ca7a4d3681ebb5166791980c636069f116aed11
TheZerth <drkainaan@gmail.com>
2025-11-19 16:49:24 -0500

    Started basis.h

--- include/ga/basis.h
+++ include/ga/basis.h
@@ -1,8 +1,15 @@
-//
-// Created by zerth on 2025-10-18.
-//
+#pragma once
 
-#ifndef GASMITH_BASIS_H
-#define GASMITH_BASIS_H
-
-#endif //GASMITH_BASIS_H
+// --- SIMPLE ---
+// A basis defines the building blocks of the algebra
+// Typically represented e1, e2, e3... eN
+// These basis are our axis. Ex: x = e1, y = e2, z = e3
+//
+// We can combine basis to construct blades
+// Blades are constructed by taking the outer product of one or more basis
+// e1 (1-vector) - a vector along axis e1
+// e2^e3 (2-vector) - an oriented plane along axis e2 e3
+// e1^e2^e3 (3-vector) - an oriented cube along axis e1 e2 e3
+// etc...
+//
+// Blades are represented using bitmasks

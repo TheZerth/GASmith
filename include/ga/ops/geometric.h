06e6a8c43d5c26cca57e6c3add498abf147041f5
TheZerth <drkainaan@gmail.com>
2025-11-20 21:09:07 -0500

    Added generalized geometric product of multivectors in geometric.h

--- include/ga/ops/geometric.h
+++ include/ga/ops/geometric.h
@@ -1,8 +1,24 @@
-//
-// Created by zerth on 2025-10-18.
-//
+#pragma once
 
-#ifndef GASMITH_GEOMETRIC_H
-#define GASMITH_GEOMETRIC_H
+Multivector geometricProduct(const Multivector& A, const Multivector& B) {
+    Multivector out(*A.alg);
+    for (BladeMask i = 0; i < (1u << A.alg->dimensions); ++i) {
+        double a = A.storage[i];
+        if (a == 0) continue;
 
-#endif //GASMITH_GEOMETRIC_H
+        Blade ba{i, +1};
+
+        for (BladeMask j = 0; j < (1u << A.alg->dimensions); ++j) {
+            double b = B.storage[j];
+            if (b == 0) continue;
+
+            Blade bb{j, +1};
+            Blade bc = geometricProductBlade(ba, bb, A.alg->signature);
+
+            if (!Blade::isZero(bc)) {
+                out.storage[bc.mask] += a * b * bc.sign;
+            }
+        }
+    }
+    return out;
+}
